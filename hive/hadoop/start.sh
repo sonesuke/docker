45ed75e3c6557b0448f3365878cd52b37e806e61
sonesuke <iamsonesuke@gmail.com>
2018-03-18 17:01:21 +0900

    hadoop added

---
+++ hive/hadoop/start.sh
@@ -0,0 +1,5 @@
+
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver
